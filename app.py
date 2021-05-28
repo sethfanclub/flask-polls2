5decc5386262d10804c6a2f2cad4decaeb47cad6
Seth Manning <sethmtwo@gmail.com>
2021-05-28 08:22:20 -0700

    5/28/2021 8:22 AM

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, g
 from flask_mysqldb import MySQL
 import yaml
+import bcrypt
+import os
 
-app = Flask(__name__)
 
 db = yaml.load(open('db.yaml'))
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -12,21 +16,49 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+  return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if g.user:
+    return redirect('/')
   if request.method == 'POST':
     username = request.form['username']
-    password = request.form['password']
+    email = request.form['email']
+    if request.form['password1'] == request.form['password2']:
+      password = request.form['password2']
+      hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    else:
+      return "Passwords didn't match"
+    
     cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, password) VALUES (%s, %s, %s)",(username, email, hashed_password))
     data = cursor.execute(f"SELECT * FROM Users WHERE (username='{username}');")
     if data:
-      user = cursor.fetchone()
-      cursor.close()
-    if password == user[2]:
-      return 'Success'
+      user = cursor.fetchall()[0]
+    mysql.connection.commit()
+    cursor.close()
+    session['user'] = user
     return redirect('/')
+
   else:
-    return render_template('index.html')
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    pass
+  else:
+    return render_template('login.html')
+
+@app.before_request
+def before_request():
+  g.user = None
+
+  if 'user' in session:
+    g.user = session['user']
 
 if __name__ == '__main__':
   app.run(debug=True)
